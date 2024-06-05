94439b1c1ed2474718290efac840f6fb49ce725d
AminoBela <belaliab1u@etu.univ-lorraine.fr>
2024-06-05 11:20:47 +0200

    Fichier fonctionnalité_V2

--- zeldiablo/test/GameLabyTest/laby/LabyrintheTest.java
+++ zeldiablo/test/GameLabyTest/laby/LabyrintheTest.java
@@ -87,4 +87,75 @@ public class LabyrintheTest {
     }
 
 
+
+    /**
+     * Test deplacement aleatoire des monstres
+     */
+
+    // Test 1: a chaque evolution (temps) le monstre se deplace, que le joueur se deplace ou non
+    // Test 4: A chaque evolution, tous les monstres tentent de se deplacer
+    // Test 5 : L'interface graphique se met a jour en fonction des deplacements des monstres
+
+    @Test
+    public void testDeplacementAleatoireMonstre() {
+        int monstreX = labyrinthe.listMonstre.get(0).getX();
+        int monstreY = labyrinthe.listMonstre.get(0).getY();
+
+        labyrinthe.deplacerMonstre(labyrinthe.listMonstre.get(0));
+
+        int nouveauMonstreX = labyrinthe.listMonstre.get(0).getX();
+        int nouveauMonstreY = labyrinthe.listMonstre.get(0).getY();
+
+        assertTrue(Math.abs(nouveauMonstreX - monstreX) <= 1 && Math.abs(nouveauMonstreY - monstreY) <= 1);
+        assertFalse(labyrinthe.getMur(nouveauMonstreX, nouveauMonstreY));
+        assertFalse(labyrinthe.pj.etrePresent(nouveauMonstreX, nouveauMonstreY));
+    }
+
+    /**
+     * Test de collision entre monstre et joueur
+     */
+
+    //Test 3 Deux monstres ou un monstre et un joueur ne peuvent pas se trouver sur la meme case
+
+    @Test
+    public void testMonstreJoueurCollision() {
+        int monstreX = labyrinthe.listMonstre.get(0).getX();
+        int monstreY = labyrinthe.listMonstre.get(0).getY();
+        int persoX = labyrinthe.pj.getX();
+        int persoY = labyrinthe.pj.getY();
+
+        // Calculer les mouvements pour aller sur la case du monstre
+        String actionVersMonstre = (monstreX > persoX) ? Labyrinthe.DROITE : (monstreX < persoX) ? Labyrinthe.GAUCHE :
+                (monstreY > persoY) ? Labyrinthe.BAS : Labyrinthe.HAUT;
+
+        labyrinthe.deplacerPerso(actionVersMonstre);
+
+        assertNotEquals(monstreX, labyrinthe.pj.getX());
+        assertNotEquals(monstreY, labyrinthe.pj.getY());
+
+    }
+
+    /**
+     * Test de collision entre monstre et obstacle
+     */
+
+    //Test 2 : • Les monstres sont bloqués par les obstacles. S'ils ont choisi de se diriger vers un
+    //obstacle (mur, joueur, ...), leur mouvement est alors annulé (sauf s'il s'agit de
+    //fantômes).
+
+    @Test
+    public void testMonstreBloqueParObstacle() {
+        int monstreX = labyrinthe.listMonstre.get(0).getX();
+        int monstreY = labyrinthe.listMonstre.get(0).getY();
+
+        // Calculer les mouvements pour aller sur la case du monstre
+        String actionVersMonstre = (monstreX > 0) ? Labyrinthe.GAUCHE : Labyrinthe.HAUT;
+
+        labyrinthe.deplacerPerso(actionVersMonstre);
+
+        assertEquals(monstreX, labyrinthe.listMonstre.get(0).getX());
+        assertEquals(monstreY, labyrinthe.listMonstre.get(0).getY());
+    }
+
+
 }
