268366006974d581d0615e5c61a92ce8e2f170bf
LukaSalvo <luka.salvo23@gmail.com>
2024-06-07 12:53:19 +0200

    Ajout de classes test + ré écriture de laby dessin

--- zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -28,12 +28,15 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour dessiner le jeu
-     * @param jeu jeu a afficher
+     *
+     * @param jeu    jeu a afficher
      * @param canvas canvas dans lequel dessiner l'etat du jeu
      */
     @Override
     public void dessinerJeu(Jeu jeu, Canvas canvas) {
         LabyJeu game = (LabyJeu) jeu;
+        Labyrinthe l = game.getLabyrinthe();
+        boolean[][] murs = l.getMurs();
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
         // Scenne de fond
@@ -41,28 +44,27 @@ public class LabyDessin implements DessinJeu {
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
         // Calcul des tailles des cellules
-        int x = game.WIDTH / game.getLabyrinthe().murs.length;
-        int y = game.HEIGHT / game.getLabyrinthe().murs[0].length;
+        int x = LabyJeu.WIDTH / murs.length;
+        int y = LabyJeu.HEIGHT / murs[0].length;
 
         // Dessin du labyrinthe
-        dessinerLabyrinthe(gc, game.getLabyrinthe().murs, x, y);
-
+        dessinerLabyrinthe(gc, murs, x, y);
+        //Dessin cases pièges
+        dessinerCasesPieges(gc, l.getCasesPieges(), x, y);
         // Dessin du personnage
-        dessinerPersonnage(gc, game.getLabyrinthe().pj, x, y);
-
+        dessinerPersonnage(gc, l.getPj(), x, y);
         // Dessin des monstres
-        dessinerMonstres(gc, game.getLabyrinthe().listMonstre, x, y);
+        dessinerMonstres(gc, l.getListMonstre(), x, y);
 
-        // Dessin des cases pièges
-       dessinerCasesPieges(gc, game.getLabyrinthe().casesPieges, x, y);
     }
 
     /**
      * Methode pour dessiner le labyrinthe
-     * @param gc objet GraphicsContext
+     *
+     * @param gc   objet GraphicsContext
      * @param murs tableau de booleens representant les murs
-     * @param x position x
-     * @param y position y
+     * @param x    position x
+     * @param y    position y
      */
     private void dessinerLabyrinthe(GraphicsContext gc, boolean[][] murs, int x, int y) {
         for (int line = 0; line < murs.length; line++) {
@@ -77,10 +79,11 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour dessiner le personnage
-     * @param gc objet GraphicsContext
+     *
+     * @param gc    objet GraphicsContext
      * @param perso objet de la classe Perso
-     * @param x position x
-     * @param y position y
+     * @param x     position x
+     * @param y     position y
      */
     private void dessinerPersonnage(GraphicsContext gc, Perso perso, int x, int y) {
         gc.setFill(getPersonnageColor(perso.getPv()));
@@ -89,14 +92,15 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour dessiner les monstres
-     * @param gc objet GraphicsContext
+     *
+     * @param gc       objet GraphicsContext
      * @param monstres liste des monstres
-     * @param x position x
-     * @param y position y
+     * @param x        position x
+     * @param y        position y
      */
     private void dessinerMonstres(GraphicsContext gc, List<Monstre> monstres, int x, int y) {
         for (Monstre monstre : monstres) {
-            if(!monstre.estMort()) {
+            if (!monstre.estMort()) {
                 gc.setFill(getMonstreColor(monstre.getPv()));
                 dessinerEntite(gc, monstre.getX(), monstre.getY(), x, y, monstre.getPv());
             }
@@ -105,28 +109,30 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour dessiner les cases pièges
-     * @param gc objet GraphicsContext
+     *
+     * @param gc          objet GraphicsContext
      * @param casesPieges liste des cases pièges
-     * @param x position x
-     * @param y position y
+     * @param x           position x
+     * @param y           position y
      */
     private void dessinerCasesPieges(GraphicsContext gc, List<CasePieges> casesPieges, int x, int y) {
         for (CasePieges c : casesPieges) {
-            if(c.getEtrePasserDessus()){
-            gc.setFill(Color.BROWN);
-            gc.fillRect(c.getPosX() * x, c.getPosY() * y, x, y);
+            if (c.getEtrePasserDessus()) {
+                gc.setFill(Color.BROWN);
+                gc.fillRect(c.getX() * x, c.getY() * y, x, y);
             }
         }
     }
 
     /**
      * Methode pour dessiner une entite
-     * @param gc objet GraphicsContext
-     * @param x position x
-     * @param y position y
-     * @param cellWidth largeur de la cellule
-     * @param cellHeight    hauteur de la cellule
-     * @param pv    points de vie
+     *
+     * @param gc         objet GraphicsContext
+     * @param x          position x
+     * @param y          position y
+     * @param cellWidth  largeur de la cellule
+     * @param cellHeight hauteur de la cellule
+     * @param pv         points de vie
      */
     private void dessinerEntite(GraphicsContext gc, int x, int y, int cellWidth, int cellHeight, int pv) {
         gc.fillOval(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
@@ -137,6 +143,7 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour obtenir la couleur de l'entite, en fonction de ses points de vie
+     *
      * @param pv points de vie
      * @return
      */
@@ -156,15 +163,16 @@ public class LabyDessin implements DessinJeu {
 
     /**
      * Methode pour obtenir la couleur du monstre, en fonction de ses points de vie
+     *
      * @param pv points de vie
      * @return
      */
     private Color getMonstreColor(int pv) {
         if (pv >= Monstre.PV_MONSTRE_ENTIER) {
             return bleuCiel;
-        } else if (pv >= Monstre.Pv_MONSTRE_DEMI) {
+        } else if (pv >= Monstre.PV_MONSTRE_DEMI) {
             return bleu;
-        } else{
+        } else {
             return bleuNoir;
         }
     }
