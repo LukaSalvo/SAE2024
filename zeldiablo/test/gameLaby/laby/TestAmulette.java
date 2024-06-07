7e88a9c27d5d898ad7b499722287a2b9e8e18c58
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-06-07 16:01:46 +0200

    Rajout d'un test

--- zeldiablo/test/gameLaby/laby/TestAmulette.java
+++ zeldiablo/test/gameLaby/laby/TestAmulette.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TestAmulette {
     @Test
     public void test() throws IOException {
@@ -15,8 +17,14 @@ public class TestAmulette {
         assertEquals(a.getX(),8);
         assertEquals(a.getY(),1);
         Perso pj = jeu.getLabyrinthe().getPj();
+        Monstre m = jeu.getLabyrinthe().getListMonstre().get(0);
+        m.deplacer(8,1);
+        jeu.getLabyrinthe().recupererAmulette();
+        a = jeu.getLabyrinthe().getAmulette();
+        assertNotNull(a);
         pj.deplacer(8,1);
         jeu.getLabyrinthe().recupererAmulette();
         assertEquals(pj.getPossedeAmulette(),true);
+
     }
 }
